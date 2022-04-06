5b0a3121bdea548122974a375b471dea44f7de94
Ben Anderson <benanderson.david@gmail.com>
2022-04-06 15:07:55 +0100

    Added SecureRandomUtil to speed up SecureRandom generation on Linux

---
+++ src/main/java/com/terraboxstudios/nanopay/SecureRandomUtil.java
@@ -0,0 +1,24 @@
+package com.terraboxstudios.nanopay;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class SecureRandomUtil {
+
+    public static SecureRandom getSecureRandom() throws NoSuchAlgorithmException {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("nix") || os.contains("nux")) {
+            try {
+                return SecureRandom.getInstance("NativePRNGNonBlocking");
+            } catch (NoSuchAlgorithmException e) {
+                try {
+                    return SecureRandom.getInstance("NativePRNG");
+                } catch (NoSuchAlgorithmException ex) {
+                    return SecureRandom.getInstanceStrong();
+                }
+            }
+        }
+        return SecureRandom.getInstanceStrong();
+    }
+
+}
